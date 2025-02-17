8efb50cf04081fa1b2cc7ff69a3f472097fcd305
nninad <nninad27@gmail.com>
2025-02-16 20:19:22 -0500

    Mastering PydanticAI: Day 1

---
+++ Day_1/01-2_pydantic_ai_simple_local.py
@@ -0,0 +1,69 @@
+from __future__ import annotations as _annotations
+
+from typing import List, Optional
+from prettytable import PrettyTable
+
+from dotenv import load_dotenv
+from pydantic import BaseModel, Field
+from pydantic_ai import Agent
+from pydantic_ai.models.openai import OpenAIModel
+
+load_dotenv("../.env")
+
+
+# Define a structured response model for a tourist place
+class TouristPlace(BaseModel):
+    name: str  # Enforces name as a string
+    description: str  # Ensures AI provides meaningful descriptions
+    zip_code: int  # Enforces zipcode as a integer
+    best_time_to_visit: str  # AI must specify the best time to visit
+    entry_fee: Optional[float] = Field(
+        None, description="Entry fee in USD, if applicable"
+    )
+    rating: float = Field(
+        ..., ge=0.0, le=5.0, description="Average rating from 0.0 to 5.0"
+    )
+
+
+ollama_model = OpenAIModel(model_name="llama3.2", base_url="http://localhost:11434/v1")
+
+
+travel_agent = Agent(
+    model=ollama_model,
+    model_settings={"temperature": 0.1},
+    result_type=List[TouristPlace],  # Enforces structured output
+    system_prompt="Provide 3 famous tourist places in the given city with descriptions, best time to visit, entry fee (if any), and average visitor rating.",
+)
+
+
+result = travel_agent.run_sync("New York City")
+print("Agent Output: \n")
+
+print(f"Data: \n {result.data} \n\n")
+print(f"Usage: \n {result.usage()} \n\n")
+
+
+# Converting result into table format
+result_table = PrettyTable()
+result_table.field_names = [
+    "Name",
+    "Description",
+    "Location - zip_code",
+    "Best Time to Visit",
+    "Entry Fee (USD)",
+    "Rating",
+]
+
+for place in result.data:
+    result_table.add_row(
+        [
+            place.name,
+            place.description,
+            place.zip_code,
+            place.best_time_to_visit,
+            "Free" if place.entry_fee is None else f"${place.entry_fee:.2f}",
+            place.rating,
+        ]
+    )
+
+print(result_table)
---
+++ README.md
@@ -0,0 +1,26 @@
+# Mastering PydanticAI: A 10-Day Journey to Building AI Agents
+
+Welcome to the **Mastering PydanticAI** series! In this series, we will systematically explore **PydanticAI**, a robust framework designed to enforce type safety and structured validation in AI-driven applications. By the end of this series, you will have the knowledge and tools required to confidently build reliable AI agents.
+
+Day 1: Build a basic AI agent using the PydanticAI framework (Type-safe and Structured Responses)
+
+Day 2: Provide additional data/services to the AI agent (Dependency Injection System)
+
+Day 3: Provide access to various tools to the AI agent
+
+Day 4: Result validation and handling exceptions
+
+Day 5: Access SQL database (Text to SQL generation)
+
+Day 6: RAG (Retrieval-Augmented Generation)
+
+Day 7: Multi-Agent Applications
+
+Day 8: Messages and Chat History
+
+Day 9: Graph
+
+Day 10: Develop end-to-end AI application
+
+
+### Rename the file example.env to .env and update the API keys in it.
